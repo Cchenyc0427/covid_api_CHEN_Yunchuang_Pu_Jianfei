c496d766fe3efbdc9bfb0c7e69e0316d59504da7
chenyunchuang <cyc08010702@gmail.com>
2024-01-03 17:18:35 +0100

    test auto

--- src/main/java/org/polytech/covid/Entity/Administrateur.java
+++ src/main/java/org/polytech/covid/Entity/Administrateur.java
@@ -1,85 +1,30 @@
 package org.polytech.covid.Entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Table
-@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "administrateur")
+@Entity(name = "administrateur")
 public class Administrateur {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "administrateur_id")
     @SequenceGenerator(name = "administrateur_id", sequenceName = "administrateur_id_seq",allocationSize = 1)
     private Integer administrateurId;
+    @Column(name = "nom")
     private String nom;
+    @Column(name = "prenom")
     private String prenom;
-
+    @Column(name = "mail")
     private String mail;
+    @Column(name = "telephone")
     private String telephone;
+    @Column(name = "password")
     private String password;
 
-    public Administrateur(Integer administrateurId, String nom, String prenom, String mail, String telephone, String password) {
-        this.administrateurId = administrateurId;
-        this.nom = nom;
-        this.prenom = prenom;
-        this.mail = mail;
-        this.telephone = telephone;
-        this.password = password;
-    }
 
-    public Administrateur() {
-    }
-
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public void setAdministrateurId(Integer administrateurId) {
-        administrateurId = administrateurId;
-    }
-
-
-
-
-    public int getAdministrateurId() {
-        return administrateurId;
-    }
-
-    public void setAdministrateurId(int id) {
-        this.administrateurId = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
 }
--- src/main/java/org/polytech/covid/Entity/Medicin.java
+++ src/main/java/org/polytech/covid/Entity/Medicin.java
@@ -1,83 +1,31 @@
 package org.polytech.covid.Entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
-@Table
-@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "medicin")
+@Entity(name = "medicin")
 public class Medicin {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "medicin_id")
     @SequenceGenerator(name = "medicin_id", sequenceName = "medicin_id_seq",allocationSize = 1)
     private int id;
+    @Column(name = "nom")
     private String nom;
+    @Column(name = "prenom")
     private String prenom;
+    @Column(name = "mail")
     private String mail;
+    @Column(name = "telephone")
     private String telephone;
-
+    @Column(name = "password")
     private String password;
 
 
 
-    public Medicin(int id, String nom, String prenom, String mail, String telephone, String password, List<rdv> rdvList) {
-        this.id = id;
-        this.nom = nom;
-        this.prenom = prenom;
-        this.mail = mail;
-        this.telephone = telephone;
-        this.password = password;
-    }
-
-    public Medicin() {
-
-    }
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
